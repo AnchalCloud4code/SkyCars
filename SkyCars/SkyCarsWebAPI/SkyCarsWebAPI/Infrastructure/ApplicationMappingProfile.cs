b67014762283c96cb793ce2bb4fcdf207baffa9f
JenishC4C <jenish@cloud4code.com>
2022-08-27 17:57:43 +0530

    .

---
+++ SkyCars/SkyCarsWebAPI/SkyCarsWebAPI/Infrastructure/ApplicationMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using System.Linq;
+
+namespace SkyCarsWebAPI.Infrastructure
+{
+    public class ApplicationMappingProfile : Profile
+    {
+        public ApplicationMappingProfile()
+        {
+           
+            //#region :: Settings ::           
+            //CreateMap<SystemMessageModel, SystemMessage>().ReverseMap().IgnoreAllPropertiesWithAnInaccessibleSetter();
+            //CreateMap<SystemSettingModel, Settings>().ReverseMap().IgnoreAllPropertiesWithAnInaccessibleSetter();
+            //#endregion
+
+        }
+    }
+}
